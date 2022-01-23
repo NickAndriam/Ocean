d2f52a21fca1eed2e506b6f4524cce3f72d7e5d4
nick-andriam <raza.jedidia@gmail.com>
2022-01-24 01:17:05 +0700

    Added Names

--- components/icon/AppIcon.js
+++ components/icon/AppIcon.js
@@ -6,7 +6,7 @@ export default function AppIcon({ icon, alt }) {
             <div className='w-24 h-24 bg-white grid place-items-center shadow-md mt-4 rounded-[20px] cursor-pointer'>
                 <img src={`/assets/icons/${icon}.svg`} alt={alt} width={50} />
             </div>
-            <h4 className='m-2 text-ocean text-lg font-thin'>Facebook</h4>
+            <h4 className='m-2 text-ocean text-lg font-thin'>{alt}</h4>
         </div>
     )
 }
